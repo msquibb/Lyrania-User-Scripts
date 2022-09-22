8224ebae28cfc6af0176f71e0bc7e692222edd0e
Mike Squibb <squibb.mike@gmail.com>
2022-09-22 00:23:23 -0400

    Added base script. Added JSFiddle to Readme.

--- Party Bag Flavor.user.js
+++ Party Bag Flavor.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Party Bag Flavor
-// @namespace    http://tampermonkey.net/
+// @namespace    https://lyrania.co.uk/
 // @version      0.1
 // @description  Make important party bag items stand out
 // @author       Ackron
@@ -11,6 +11,44 @@
 
 (function() {
     'use strict';
-
-    // Your code here...
+    const autoRegEx = /autos/;
+    const jboxRegex = /A shiny box of jewels!/;
+    const tokensRegex = /tokens/;
+    const orbsRegex = /Craftsman's Orb/;
+    const dpRegex = /dungeon points/;
+    const numberRegex = /\D+/g;
+    var autos = 0;
+    var jbox = 0;
+    var tokens = 0;
+    var orbs = {poor: 0, decent: 0, fine: 0, quality: 0};
+    var dp = 0;
+    var presents = document.getElementsByClassName('VibbleBdayOpened')
+    for (var i = 0; i < presents.length; i++){
+        var present = presents[i];
+        var presentDesc = present.textContent;
+        if (presentDesc.match(autoRegEx)) {
+            present.setAttribute('style', 'color: #FF0000; font-weight: bold');
+            autos += parseInt(presentDesc.replace(numberRegex, ''));
+          } else if (presentDesc.match(jboxRegex)) {
+            present.setAttribute('style', 'color: #FFFF00; font-weight: bold');
+            jbox++;
+          } else if (presentDesc.match(tokensRegex)) {
+            tokens += parseInt(presentDesc.replace(numberRegex, ''));
+          } else if (presentDesc.match(orbsRegex)) {
+            present.setAttribute('style', 'color: #FF0000; font-weight: bold');
+          } else if (presentDesc.match(dpRegex)) {            
+            dp += parseInt(presentDesc.replace(numberRegex, ''));
+          }
+    }
+    var header = document.getElementsByClassName('PartyBagHeaderMessage');
+    var report = document.getElementById('ackron-party-report');
+    if (report){
+        console.log('We have the report div already');
+    } else {
+        var newReport = document.createElement('div');
+        newReport.setAttribute('id', 'ackron-party-report');
+        header[0].insertAdjacentElement('afterend', newReport);
+        report = newReport;
+    }
+    report.innerHTML = `<ul><li>Jewel Boxes: ${jbox}</li><li>Autos: ${autos}</li><li>Tokens: ${tokens}</li><li>Dungeon Points: ${dp}</li><li>Orbs: 0</li></ul>`
 })();
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Lyrania-User-Scripts
+# Ackron's Lyrania User Scripts
  Just some various user scripts that I use while playing Lyrania to improve the play experience. 
 
  No, there are no hacks or cheats in here, go take that trash somewhere else.
+
+## Party Bag Flavor
+
+This script reads through all of the rewards that you get from opening multiple pink party bags, and keeps a running count of the important items (autos, tokens, dungeon points, orbs and jewelry boxes), highlights some of them in the list, and then adds a summary at the top.
+
+ To see an example of how this functions, look at [this JSFiddle](https://jsfiddle.net/Ackron/trhusLng/31/).
