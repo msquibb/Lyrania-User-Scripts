e1341a1ecaac61deb76b571dbfbabf880beb3400
Mike Squibb <squibb.mike@gmail.com>
2022-09-21 22:17:49 -0400

    Create Party Bag Flavor.user.js

---
+++ Party Bag Flavor.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Party Bag Flavor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Make important party bag items stand out
+// @author       Ackron
+// @match        https://lyrania.co.uk/use-vibblez-birthday-item.php
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
