1b8d255a6d46f69e3c59275bfa73c94e6bc04a69
Mike Squibb <squibb.mike@gmail.com>
2025-03-23 19:47:44 -0400

    Bump version to 0.3.0

--- Party Bag Flavor.user.js
+++ Party Bag Flavor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Party Bag Flavor
 // @namespace    https://thesilvertower.net/
-// @version      0.2.4
+// @version      0.3.0
 // @updateURL    https://github.com/msquibb/Lyrania-User-Scripts/raw/main/Party%20Bag%20Flavor.user.js
 // @description  Make important party bag items stand out
 // @author       Ackron
