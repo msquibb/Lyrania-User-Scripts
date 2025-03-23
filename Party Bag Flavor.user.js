2e57a9d0ccf5355b8f6e265b1729c3f395b54829
Mike Squibb <squibb.mike@gmail.com>
2025-03-23 19:45:03 -0400

    Add tracking for Dragon Eggs and bonus Procs in Party Bag Flavor.
    Additional orb tracking.

--- Party Bag Flavor.user.js
+++ Party Bag Flavor.user.js
@@ -17,7 +17,15 @@
   const jboxRegex = /A shiny box of jewels!/;
   const tokensRegex = /tokens/;
   const orbsRegex = /Craftsman's Orb/;
+  const qualityOrbRegex = /Quality/;
+  const fineOrbRegex = /Fine/;
+  const decentOrbRegex = /Decent/;
+  const poorOrbRegex = /Poor/;
   const dpRegex = /dungeon points/;
+  const dEggRegex = /Dragon Egg/;
+  const decProcRegex = /Dec! Dec! Dec!/;
+  const quadProcRegex = /enough quad to last/;
+  const jewelsRegex = /Everybody gets a jewel!/;
   const numberRegex = /\D+/g;
 
   var popupDiv = document.getElementById("popupresdisplay");
@@ -41,6 +49,10 @@
     var tokens = 0;
     var orbs = { poor: 0, decent: 0, fine: 0, quality: 0 };
     var dp = 0;
+    var degg = 0;
+    var quadProc = 0;
+    var decProc = 0;
+    var jewels = 0;
     var presents = document.getElementsByClassName('VibbleBdayOpened');
     console.info(presents);
     for (var i = 0; i < presents.length; i++) {
@@ -55,9 +67,27 @@
       } else if (presentDesc.match(tokensRegex)) {
         tokens += parseInt(presentDesc.replace(numberRegex, ''));
       } else if (presentDesc.match(orbsRegex)) {
-        present.setAttribute('style', 'color: #FF0000; font-weight: bold');
+        if (presentDesc.match(qualityOrbRegex)){
+          orbs.quality++;
+        } else if (presentDesc.match(fineOrbRegex)) {
+          orbs.fine++;
+        } else if (presentDesc.match(decentOrbRegex)) {
+          orbs.decent++;
+        } else if (presentDesc.match(poorOrbRegex)) {
+          orbs.poor++;
+        }
+        present.setAttribute('style', 'color:rgb(0, 238, 255); font-weight: bold');
       } else if (presentDesc.match(dpRegex)) {
         dp += parseInt(presentDesc.replace(numberRegex, ''));
+      } else if (presentDesc.match(dEggRegex)) {
+        degg += parseInt(presentDesc.replace(numberRegex, ''));
+        present.setAttribute('style', 'color: #00DD00; font-weight: bold;')
+      } else if (presentDesc.match(quadProcRegex)) {
+        quadProc += parseInt(presentDesc.replace(numberRegex, ''));
+      } else if (presentDesc.match(decProcRegex)){
+        decProc += parseInt(presentDesc.replace(numberRegex, ''));
+      } else if (presentDesc.match(jboxRegex)){
+        jewels += parseInt(presentDesc.replace(numberRegex, ''));
       }
     }
     var header = document.getElementsByClassName('PartyBagHeaderMessage');
@@ -72,8 +102,31 @@
         header[0].insertAdjacentElement('afterend', newReport);
         report = newReport;
       }
-      report.innerHTML = `<ul><li>Jewel Boxes: ${jbox}</li><li>Autos: ${autos}</li><li>Tokens: ${tokens}</li><li>Dungeon Points: ${dp}</li><li>Orbs: 0</li></ul>`
-      var rpt = { 'autos': autos, 'jbox': jbox, 'orbs': orbs, 'tokens': tokens, 'dp': dp };
+      report.innerHTML = `<ul>
+        <li>Jewel Boxes: ${jbox}</li>
+        <li>Autos: ${autos}</li>
+        <li>Tokens: ${tokens}</li>
+        <li>Dungeon Points: ${dp}</li>
+        <li>Dragon Eggs: ${degg}</li>
+        <li>Quad Procs: ${quadProc}</li>
+        <li>Decuple Procs: ${decProc}</li>
+        <li>Orbs: ${orbs.quality + orbs.decent + orbs.fine + orbs.poor}<ul>
+          <li>Quality: ${orbs.quality}</li>
+          <li>Fine: ${orbs.fine}</li>
+          <li>Decent: ${orbs.decent}</li>
+          <li>Poor: ${orbs.poor}</li>
+        </ul></li>
+      </ul>`;
+      var rpt = { 
+        'autos': autos, 
+        'jbox': jbox, 
+        'orbs': orbs, 
+        'tokens': tokens, 
+        'dp': dp, 
+        'eggs': degg, 
+        'quad': quadProc, 
+        'decuple': decProc 
+      };
       console.info(rpt);
     }
     observe();
